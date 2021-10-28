8494793affaaccabde5575afcf203c91aeac3789
Oleg Kanatov <olegkanatov7@gmail.com>
2021-10-28 14:36:50 +0300

    GET Requests

---
+++ Networking/ImageViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ImageViewController.swift
+//  Networking
+//
+//  Created by Oleg Kanatov on 28.10.21.
+//
+
+import UIKit
+
+class ImageViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var activityIndicator: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+        fetchImage()
+    }
+    
+    func fetchImage() {
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { (data, response, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageView.image = image
+                }
+            }
+        }.resume()
+    }
+    
+}
--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -8,41 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var label: UILabel!
-    @IBOutlet weak var getImageButton: UIButton!
-    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    @IBAction func getRequest(_ sender: Any) {
         
-        activityIndicator.isHidden = true
-        activityIndicator.hidesWhenStopped = true
-    }
-    
-    @IBAction func getImagePressed(_ sender: Any) {
-        
-        label.isHidden = true
-        getImageButton.isEnabled = false
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-        
-        guard let url = URL(string: "https://applelives.com/wp-content/uploads/2016/03/iPhone-SE-11.jpeg") else { return }
-        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+                
         let session = URLSession.shared
-        
         session.dataTask(with: url) { data, responce, error in
-            if let data = data, let image = UIImage(data: data) {
-                DispatchQueue.main.async {
-                    self.activityIndicator.stopAnimating()
-                    self.imageView.image = image
-                }
+            
+            guard let responce = responce, let data = data else { return }
+            print(responce)
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
             }
         }.resume()
     }
     
-
-
+    @IBAction func postRequest(_ sender: Any) {
+        
+    }
+    
+    
+    
 }
 
