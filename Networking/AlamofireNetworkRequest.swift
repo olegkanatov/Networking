438d6ee65455f836e8085a8480394791ae33669b
Oleg Kanatov <olegkanatov7@gmail.com>
2021-11-01 14:13:44 +0300

    add Request customization

--- Networking/AlamofireNetworkRequest.swift
+++ Networking/AlamofireNetworkRequest.swift
@@ -14,8 +14,15 @@ class AlamofireNetworkRequest {
         
         guard let url = URL(string: url) else { return }
         
-        AF.request(url, method: .get).responseJSON { responce in
-            print(responce)
+        AF.request(url, method: .get).validate().responseJSON { (response) in
+            
+            switch response.result {
+                
+            case .success(let value):
+                print(value)
+            case .failure(let error):
+                print(error)
+            }
         }
     }
 }
